aa886372677ecdc8ba3741cfbd4cc50e1051d7a3
Damaris <154326835+DamiMocch@users.noreply.github.com>
2025-08-06 00:37:07 -0600

    curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -2,7 +2,8 @@
 document.getElementById("download-btn").addEventListener("click", function () {
     // Crear un enlace dinámico para descargar el archivo
     const link = document.createElement("a");
-    link.href = "assets/Damaris_MoctezumaChimal_CV-3.pdf"; // Reemplaza con la ruta real de tu archivo PDF
-    link.download = "Damaris_MoctezumaChimal_CV-3.pdf"; // Nombre sugerido para el archivo descargado
+    link.href = "assets/CurrículumVitae_DamarisMoctezuma.pdf.pdf.pdf"; // Reemplaza con la ruta real de tu archivo PDF
+    link.download = "CurrículumVitae_DamarisMoctezuma.pdf.pdf.pdf"; // Nombre sugerido para el archivo descargado
     link.click(); // Simula un clic para iniciar la descarga
+
   });
