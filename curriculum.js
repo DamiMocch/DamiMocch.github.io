e028d1d39f924d2964f0fd535036399059996fc1
Damaris <154326835+DamiMocch@users.noreply.github.com>
2025-08-06 00:39:09 -0600

    Update curriculum.js

--- curriculum.js
+++ curriculum.js
@@ -2,8 +2,9 @@
 document.getElementById("download-btn").addEventListener("click", function () {
     // Crear un enlace dinámico para descargar el archivo
     const link = document.createElement("a");
-    link.href = "assets/CurrículumVitae_DamarisMoctezuma.pdf.pdf.pdf"; // Reemplaza con la ruta real de tu archivo PDF
-    link.download = "CurrículumVitae_DamarisMoctezuma.pdf.pdf.pdf"; // Nombre sugerido para el archivo descargado
+    link.href = "assets/CurrículumVitae_DamarisMoctezuma.pdf"; // Reemplaza con la ruta real de tu archivo PDF
+    link.download = "CurrículumVitae_DamarisMoctezuma.pdf"; // Nombre sugerido para el archivo descargado
     link.click(); // Simula un clic para iniciar la descarga
 
   });
+
